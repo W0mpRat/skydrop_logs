56605b9f528869d157932627dd57c21c7ef96994
Chase Anderson <anderson.chasew@gmail.com>
2020-09-08 08:31:16 -0600

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const xlsx = require('xlsx')
 const fsp = require('fs').promises
 const fs = require('fs')
-const path = 'F:/LOGS/'
+const path = 'H:/LOGS/'
+const IGCParser = require('igc-parser');
 // const path = './flight_logs/
 
 async function main () {
@@ -20,6 +21,7 @@ async function main () {
           // console.log(flight_file)
           const regex = /(?<=LXSB  SKYDROP-DURATION-ms: )\d+(?= )/;
           if ((m = regex.exec(flight_file)) !== null) {
+            let result = IGCParser.parse(flight_file);
 
             m.forEach((match, groupIndex) => {
               if (parseInt(m) < 10000) {
@@ -80,4 +82,24 @@ function getFileNameFormattedDate(date) {
   let day = date.getDate().toString().padStart(2, '0');
 
   return month + '_' + day + '_' + year;
+}
+
+function calcCrow (coords1, coords2) {
+  // var R = 6.371; // km
+  var R = 6371000
+  var dLat = toRad(coords2.lat - coords1.lat)
+  var dLon = toRad(coords2.lng - coords1.lng)
+  var lat1 = toRad(coords1.lat)
+  var lat2 = toRad(coords2.lat)
+
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  var d = R * c
+  return d
+}
+
+// Converts numeric degrees to radians
+function toRad (Value) {
+  return Value * Math.PI / 180
 }
